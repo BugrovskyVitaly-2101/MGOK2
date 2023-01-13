ec151f5444ce3afb6f7166ffa1bfd3fd77441478
Бугровский Виталий (15.ИСиП.21.01) <118171726+BugrovskyVitaly-2101@users.noreply.github.com>
2023-01-13 17:18:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+
+public class Main {
+static Scanner scanner = new Scanner(System.in);
+
+public static void main (String[] args) {
+    menu();
+     logic();
+	}
+public static void menu(){
+    System.out.println(int + "\n" + 1)
+    
+}
+public static void logic(){
+  int a = 7;
+int b = 3;
+if c != 0 >;
+int c = b + a
+System.out.println()
+}
+switch(a){
+    case 1:
+        int sum = b + a;
+        System.out.println(5 + sum);
+    break
+case 2:
+    int raz = b - a;
+    System.out.println(5 + raz);
+break
+    
+case 3:
+    int umn = b * a;
+System.out.println (a + umn);
+return
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
